a4541c84b21e391a1f20441b973f3f768d2fe409
dkimpara <dhammakimpara@gmail.com>
2020-08-14 17:19:39 -0600

    finished theory prelim

--- run.py
+++ run.py
@@ -6,9 +6,7 @@ import numpy as np
 if __name__ == '__main__':
     #collect_data([20,30,40], np.linspace(2.05, 2.45, 5))
     #collect_data([20,30,40], np.linspace(2.23, 2.26, 2))
-    for m in [50, 60, 70]:
+    for m in [85, 100]:
         startTime = datetime.now()
         collect_data([m], np.linspace(2.1, 2.5, 9))
-        collect_data([m], [2.225, 2.275])
         print(datetime.now() - startTime)
-    #collect_data([85,100], np.linspace(2.1, 2.5, 9))
